abe0498491887fceb2c8d0db9d1c419ee4444731
CapybaraBread <denis05102012@gmail.com>
2025-11-13 15:46:25 +0000

    add m2m inline to show owners on flat page

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class FlatOwnersInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ['owner']
+    extra = 0
+
+
 class HausAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'price', 'owner')
     readonly_fields = ['created_at']
@@ -19,8 +25,8 @@ class HausAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building']
     raw_id_fields = ["how_much_like"]
+    inlines = [FlatOwnersInline]
 
 admin.site.register(Complaint)
 admin.site.register(Flat, HausAdmin)
 admin.site.register(Owner)
-
